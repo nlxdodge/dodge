3d2a4a2c819b1a0db28a58b4e4dcfa5d1a1d2b6f
Gerben Veenhof <nlxdodge@gmail.com>
2020-07-05 22:34:46 +0200

    Added 50 percent change to damage weapon used against enchant

--- src/main/java/nl/nlxdodge/scrap_yard/ScrapyardMod.java
+++ src/main/java/nl/nlxdodge/scrap_yard/ScrapyardMod.java
@@ -6,6 +6,7 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.MaterialColor;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -18,6 +19,7 @@ import nl.nlxdodge.scrap_yard.blocks.ScrapBlock;
 import nl.nlxdodge.scrap_yard.items.weapons.BambooSpear;
 import nl.nlxdodge.scrap_yard.items.weapons.GlassSword;
 import nl.nlxdodge.scrap_yard.misc.ToolMaterials;
+import nl.nlxdodge.scrap_yard.misc.enchants.CorrosiveEnchant;
 
 public class ScrapyardMod implements ModInitializer {
 
@@ -26,16 +28,20 @@ public class ScrapyardMod implements ModInitializer {
 
 	public static final Item BAMBOO_SPEAR = new BambooSpear(ToolMaterials.BAMBOO, 2, 1.0f,
 			new Item.Settings().group(SCRAP_YARD_GROUP));
+	public static final Item GLASS_SWORD = new GlassSword(ToolMaterials.GLASS, 2, 1.5f,
+			new Item.Settings().group(SCRAP_YARD_GROUP));
+	public static final Block SCRAP_BLOCK = new ScrapBlock(AbstractBlock.Settings.of(Material.METAL, MaterialColor.BROWN)
+			.strength(1.0F).sounds(BlockSoundGroup.SCAFFOLDING));
 
-	public static final Item GLASS_SWORD = new GlassSword(ToolMaterials.GLASS, 2, 1.5f, new Item.Settings().group(SCRAP_YARD_GROUP));
-	public static final Block SCRAP_BLOCK = new ScrapBlock(
-			AbstractBlock.Settings.of(Material.METAL, MaterialColor.BROWN).strength(1.0F).sounds(BlockSoundGroup.LODESTONE));
+	public static final Enchantment CORROSIVE = Registry.register(Registry.ENCHANTMENT,
+			new Identifier("scrap_yard", "corrosive"), new CorrosiveEnchant());
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("scrap_yard", "glass_sword"), GLASS_SWORD);
 		Registry.register(Registry.ITEM, new Identifier("scrap_yard", "bamboo_spear"), BAMBOO_SPEAR);
 		Registry.register(Registry.BLOCK, new Identifier("scrap_yard", "scrap"), SCRAP_BLOCK);
+		Registry.register(Registry.BLOCK, new Identifier("scrap_yard", "scrap_block"), SCRAP_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier("scrap_yard", "scrap_block"),
 				new BlockItem(SCRAP_BLOCK, new Item.Settings().group(SCRAP_YARD_GROUP)));
 	}
